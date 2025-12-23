c7c16ebceb81dfe6173fe0959243860b2a38a384
Benhammadi Lokmane <l.benhammadi@esi-sba.dz>
2025-12-23 12:23:57 +0100

    Enhanced Docker

---
+++ docker/deploy/build.sh
@@ -0,0 +1,4 @@
+cp frontend/.env ../../frontend/.env
+cp backend/.env ../../backend/.env
+cp backend/settings.py ../../backend/core/settings.py
+cp backend/requirements.txt ../../backend/requirements.txt
---
+++ docker/dev/build.sh
@@ -0,0 +1,5 @@
+cp frontend/.env ../../frontend/.env
+cp backend/.env ../../backend/.env
+cp backend/settings.py ../../backend/core/settings.py
+cp backend/requirements.txt ../../backend/requirements.txt
+
